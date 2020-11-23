903c354f57ec484539ffffede9658689362fd34d
Kleydson Barros Vieira <Kleydson122@gmail.com>
2020-11-23 12:01:02 -0300

    commit inicial

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,7 @@
+@include('layout.includes.head')
+            
+{{-- aqui acho que vou colocar um possivel nav bar --}}
+
+@yield('content')            
+
+@include('layout.includes.footer')
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+
+@section('title', 'home')
+
+@section('content')
+
+
+
+
+
+@endsection
